6ce5b6e267db7bdfc29b4db61df047b466f3e19a
Forest Nenninger <forestnenninger@Forests-MacBook-Pro.local>
2023-01-29 17:12:29 -0600

    Just floated the inputs on the same line of code rather than creating additional variables for the floats.

--- payCalculator.py
+++ payCalculator.py
@@ -2,11 +2,9 @@ def calculatePay():
     # Implement your solution in between the two comment blocks
     print("calculating pay")
     # This first line is provided for you
-    hrs = input("Enter Hours: ")
-    rate = input('Enter Rate: ')
-    nhrs = float(hrs)
-    nrate = float(rate)
-    r = nhrs*nrate
+    hrs = float(input("Enter Hours: "))
+    rate = float(input('Enter Rate: '))
+    r = hrs*rate
     print(r)
 
 
